27e034fd7bedea0b8b9d97d8cfd8c1c693cf0efb
helenwang <helenwang@flareon.cs.uchicago.edu>
2015-01-16 12:57:02 -0600

    Initial Commit

---
+++ It's A Zoo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  It's A Zoo
+//
+//  Created by helenwang on 1/16/15.
+//  Copyright (c) 2015 helenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ It's A Zoo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  It's A Zoo
+//
+//  Created by helenwang on 1/16/15.
+//  Copyright (c) 2015 helenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
